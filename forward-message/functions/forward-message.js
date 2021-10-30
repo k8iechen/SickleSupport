b8faaf90419144a52eb42b5c306096e51de6ce99
MannDP <patelmann1999@gmail.com>
2021-10-30 18:23:29 -0400

    bootstrap project

--- forward-message/functions/forward-message.js
+++
@@ -1,7 +0,0 @@
-exports.handler = function (context, event, callback) {
-  const twiml = new Twilio.twiml.MessagingResponse();
-  twiml.message(`From: ${event.From}. Body: ${event.Body}`, {
-    to: context.MY_PHONE_NUMBER,
-  });
-  callback(null, twiml);
-};
