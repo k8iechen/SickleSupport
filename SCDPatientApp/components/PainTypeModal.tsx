a2815a6a2a0d36078e7165c2debf83222b11740f
Dhvani Patel <dhvani.patel@uwaterloo.ca>
2021-11-28 18:15:45 -0500

    refactor modal

---
+++ SCDPatientApp/components/PainTypeModal.tsx
@@ -0,0 +1,129 @@
+import React from "react";
+import { StyleSheet, Text, TouchableOpacity } from "react-native";
+import { VStack, HStack, Center, Modal } from "native-base";
+
+import { Feather } from "@expo/vector-icons";
+import { AntDesign } from "@expo/vector-icons";
+import { MaterialIcons } from "@expo/vector-icons";
+
+type TPainTypeModalProps = {
+  showModal: boolean;
+  setShowModal;
+  setShowWhenModal;
+  navigation;
+};
+
+const PainTypeModal = ({
+  showModal,
+  setShowModal,
+  setShowWhenModal,
+  navigation,
+}: TPainTypeModalProps) => {
+  return (
+    <Modal isOpen={showModal} onClose={setShowModal} size="lg">
+      <Modal.Content style={styles.modalContent}>
+        <Modal.Header>
+          <Center>
+            <Text style={styles.modalHeader}>
+              When did you have the pain episode?
+            </Text>
+          </Center>
+        </Modal.Header>
+        <Modal.Body>
+          <VStack>
+            <TouchableOpacity
+              style={styles.ongoingBtn}
+              onPress={() => {
+                setShowModal(false);
+                navigation.navigate("PainEpisodeFormScreen");
+              }}
+            >
+              <HStack>
+                <AntDesign
+                  name="exclamationcircleo"
+                  size={24}
+                  color="#FF0000"
+                  style={styles.btnIcon}
+                />
+                <Text style={styles.btnText}>Ongoing episode</Text>
+                <MaterialIcons
+                  name="keyboard-arrow-right"
+                  size={24}
+                  color="#FF0000"
+                  style={styles.btnArrow}
+                />
+              </HStack>
+            </TouchableOpacity>
+            <TouchableOpacity
+              style={styles.pastBtn}
+              onPress={() => {
+                setShowWhenModal(true);
+              }}
+            >
+              <HStack>
+                <Feather
+                  name="clock"
+                  size={24}
+                  color="#FFC702"
+                  style={styles.btnIcon}
+                />
+                <Text style={styles.btnText}>Record a past episode</Text>
+                <MaterialIcons
+                  name="keyboard-arrow-right"
+                  size={24}
+                  color="#FFC702"
+                  style={styles.btnArrow}
+                />
+              </HStack>
+            </TouchableOpacity>
+          </VStack>
+        </Modal.Body>
+      </Modal.Content>
+    </Modal>
+  );
+};
+
+const styles = StyleSheet.create({
+  modalContent: {
+    marginBottom: 0,
+    marginTop: "auto",
+    width: "100%",
+    backgroundColor: "#EDEBF4",
+    borderRadius: 24,
+  },
+  modalHeader: {
+    fontFamily: "Poppins-SemiBold",
+    fontSize: 18,
+    marginTop: 12,
+    marginLeft: 5,
+  },
+  ongoingBtn: {
+    height: 73,
+    backgroundColor: "#ffffff",
+    borderRadius: 24,
+    marginBottom: 20,
+    justifyContent: "center",
+  },
+  pastBtn: {
+    height: 73,
+    backgroundColor: "#ffffff",
+    borderRadius: 24,
+    marginBottom: 30,
+    justifyContent: "center",
+  },
+  btnIcon: {
+    marginLeft: 31,
+    marginRight: 20,
+  },
+  btnText: {
+    fontFamily: "Poppins-Medium",
+    fontSize: 16,
+    color: "#0A0909",
+  },
+  btnArrow: {
+    marginLeft: "auto",
+    marginRight: 25,
+  },
+});
+
+export default PainTypeModal;
--- SCDPatientApp/navigation/tabs.tsx
+++ SCDPatientApp/navigation/tabs.tsx
@@ -21,9 +21,6 @@ import {
   Modal,
   Button,
 } from "native-base";
-import { Feather } from "@expo/vector-icons";
-import { AntDesign } from "@expo/vector-icons";
-import { MaterialIcons } from "@expo/vector-icons";
 import { Entypo } from "@expo/vector-icons";
 import Colors from "../constants/Colors";
 
@@ -37,7 +34,7 @@ import CustomTabBarButton from "../components/CustomTabBarComponent";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import { RootStackParamList } from "../models/navigation";
 import DailyDiaryFormScreen from "../screens/DailyDiaryFormScreen";
-import { values } from "mobx";
+import PainTypeModal from "../components/PainTypeModal";
 
 const NewPainEpisodePlaceholder = () => <View />;
 
@@ -408,125 +405,12 @@ const Tabs = () => {
           </Modal.Body>
         </Modal.Content>
       </Modal>
-      <Modal
-        isOpen={showTypePainEpisodeModal}
-        onClose={setShowTypePainEpisodeModal}
-        size="lg"
-      >
-        <Modal.Content
-          style={{
-            marginBottom: 0,
-            marginTop: "auto",
-            width: "100%",
-            backgroundColor: "#EDEBF4",
-            borderRadius: 24,
-          }}
-        >
-          <Modal.Header>
-            <Center>
-              <Text
-                style={{
-                  fontFamily: "Poppins-SemiBold",
-                  fontSize: 18,
-                  marginTop: 12,
-                  marginLeft: 5,
-                }}
-              >
-                When did you have the pain episode?
-              </Text>
-            </Center>
-          </Modal.Header>
-          <Modal.Body>
-            <VStack>
-              <TouchableOpacity
-                style={{
-                  height: 73,
-                  backgroundColor: "#ffffff",
-                  borderRadius: 24,
-                  marginBottom: 20,
-                  justifyContent: "center",
-                }}
-                onPress={() => {
-                  setShowTypePainEpisodeModal(false);
-                  navigationObj.navigate("PainEpisodeFormScreen");
-                }}
-              >
-                <HStack>
-                  <AntDesign
-                    name="exclamationcircleo"
-                    size={24}
-                    color="#FF0000"
-                    style={{
-                      marginLeft: 31,
-                      marginRight: 20,
-                    }}
-                  />
-                  <Text
-                    style={{
-                      fontFamily: "Poppins-Medium",
-                      fontSize: 16,
-                      color: "#0A0909",
-                    }}
-                  >
-                    Ongoing episode
-                  </Text>
-                  <MaterialIcons
-                    name="keyboard-arrow-right"
-                    size={24}
-                    color="#FF0000"
-                    style={{
-                      marginLeft: "auto",
-                      marginRight: 25,
-                    }}
-                  />
-                </HStack>
-              </TouchableOpacity>
-              <TouchableOpacity
-                style={{
-                  height: 73,
-                  backgroundColor: "#ffffff",
-                  borderRadius: 24,
-                  marginBottom: 30,
-                  justifyContent: "center",
-                }}
-                onPress={() => {
-                  setShowWhenPainEpisodeModal(true);
-                }}
-              >
-                <HStack>
-                  <Feather
-                    name="clock"
-                    size={24}
-                    color="#FFC702"
-                    style={{
-                      marginLeft: 31,
-                      marginRight: 20,
-                    }}
-                  />
-                  <Text
-                    style={{
-                      fontFamily: "Poppins-Medium",
-                      fontSize: 16,
-                      color: "#0A0909",
-                    }}
-                  >
-                    Record a past episode
-                  </Text>
-                  <MaterialIcons
-                    name="keyboard-arrow-right"
-                    size={24}
-                    color="#FFC702"
-                    style={{
-                      marginLeft: "auto",
-                      marginRight: 25,
-                    }}
-                  />
-                </HStack>
-              </TouchableOpacity>
-            </VStack>
-          </Modal.Body>
-        </Modal.Content>
-      </Modal>
+      <PainTypeModal
+        showModal={showTypePainEpisodeModal}
+        setShowModal={setShowTypePainEpisodeModal}
+        setShowWhenModal={setShowWhenPainEpisodeModal}
+        navigation={navigationObj}
+      />
       <Navigator
         screenOptions={{
           tabBarShowLabel: false,
