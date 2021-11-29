d0865fccb00020548c340c6d9b8fb3530e63df9d
Dhvani Patel <dhvani.patel@uwaterloo.ca>
2021-11-28 23:22:35 -0500

    add greeting and summary card

--- SCDPatientApp/screens/HomeScreen.tsx
+++ SCDPatientApp/screens/HomeScreen.tsx
@@ -1,27 +1,38 @@
 import * as React from "react";
-import { Text, View, StyleSheet } from "react-native";
-import { Button } from "native-base";
-import EditScreenInfo from "../components/EditScreenInfo";
+import { Text, View, StyleSheet, Image } from "react-native";
+import { Button, VStack, Box, HStack } from "native-base";
 import { RootTabScreenProps } from "../models/navigation";
 import Colors from "../constants/Colors";
+import SummaryCard from "../components/SummaryCard";
 
 export default function HomeScreen({
   navigation,
 }: RootTabScreenProps<"HomeScreen">) {
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>Aowkin Home Screen</Text>
-      <View style={styles.separator} />
-      <Button
-        size="sm"
-        colorScheme="light"
-        style={styles.diaryButton}
-        variant="outline"
-        onPress={() => navigation.navigate("DailyDiaryFormScreen")}
-      >
-        <Text style={styles.diaryButtonText}>Complete Diary Entry</Text>
-      </Button>
-      <EditScreenInfo path="/screens/HomeScreen.tsx" />
+      <VStack style={styles.content}>
+        <Text style={styles.helloText}>Hello,</Text>
+        <Text style={styles.nameText}>Katherine</Text>
+
+        <SummaryCard
+          medicationStreak={8}
+          totalPainEpisodes={8}
+          numHospitalEpisodes={1}
+        />
+
+        <HStack style={styles.historyBox}>
+          <Text style={styles.historyText}>History</Text>
+          <Button
+            size="sm"
+            colorScheme="light"
+            style={styles.diaryButton}
+            variant="outline"
+            onPress={() => navigation.navigate("DailyDiaryFormScreen")}
+          >
+            <Text style={styles.diaryButtonText}>Complete Diary Entry</Text>
+          </Button>
+        </HStack>
+      </VStack>
     </View>
   );
 }
@@ -29,8 +40,10 @@ export default function HomeScreen({
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    alignItems: "center",
-    justifyContent: "center",
+    margin: 30,
+  },
+  content: {
+    marginTop: 45,
   },
   title: {
     fontSize: 20,
@@ -42,6 +55,7 @@ const styles = StyleSheet.create({
     width: "80%",
   },
   diaryButton: {
+    marginLeft: "auto",
     fontFamily: "Poppins-Regular",
     borderColor: Colors.darkGrey,
     borderRadius: 6,
@@ -58,5 +72,20 @@ const styles = StyleSheet.create({
     color: Colors.darkGrey,
     borderColor: Colors.darkGrey,
     borderRadius: 6,
+  },
+  historyBox: {
+    marginTop: 62,
+  },
+  helloText: {
+    fontFamily: "Poppins-SemiBold",
+    fontSize: 28,
+  },
+  nameText: {
+    fontFamily: "Poppins-Regular",
+    fontSize: 28,
+  },
+  historyText: {
+    fontFamily: "Poppins-Medium",
+    fontSize: 17,
   },
 });
