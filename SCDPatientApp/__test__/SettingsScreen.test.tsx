28cd18e7415e4090c57aa50d8787f41c6c3a9c63
Thomas McKee <caffeine.storm@gmail.com>
2022-07-12 15:30:03 -0400

    Add a fix for NativeBase tests and check that the 'back button' shows up on the settings screen

--- SCDPatientApp/__test__/SettingsScreen.test.tsx
+++ SCDPatientApp/__test__/SettingsScreen.test.tsx
@@ -1,12 +1,41 @@
 import * as React from 'react';
 import {
-  render,
+  NativeBaseProvider
+} from 'native-base';
+import {
+  render as native_render,
+  screen,
 } from '@testing-library/react-native';
+
 import SettingsScreen from '../screens/SettingsScreen';
 
+const render = (comp, options) => {
+  const inset = {
+    frame: { x: 0, y: 0, width: 0, height: 0 },
+    insets: { top: 0, left: 0, right: 0, bottom: 0 },
+  };
+  return native_render(
+    <NativeBaseProvider initialWindowMetrics={inset}>
+      {comp}
+    </NativeBaseProvider>
+  );
+};
+
 test('can render', () => {
-  const root = render (
+  const root = render(
     <SettingsScreen/>
   );
   expect(root).toBeTruthy();
+});
+
+
+test('expected elements are rendered', () => {
+  render(
+    <SettingsScreen/>
+  );
+  const backButton = screen.getByLabelText("go-back");
+  expect(backButton).toBeTruthy();
+
+  const settingsHeader = screen.getByText("Settings");
+  expect(settingsHeader).toBeTruthy();
 });
