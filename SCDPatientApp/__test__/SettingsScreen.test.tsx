e3c326d4e0e42f39b9e30eb484610bb9a9a51a1f
Thomas McKee <caffeine.storm@gmail.com>
2022-07-14 22:02:22 -0400

    Clean up unused parameter

--- SCDPatientApp/__test__/SettingsScreen.test.tsx
+++ SCDPatientApp/__test__/SettingsScreen.test.tsx
@@ -9,7 +9,7 @@ import {
 
 import SettingsScreen from '../screens/SettingsScreen';
 
-const render = (comp, options) => {
+const render = (comp) => {
   const inset = {
     frame: { x: 0, y: 0, width: 0, height: 0 },
     insets: { top: 0, left: 0, right: 0, bottom: 0 },
