087c0614e33cb87ef3aa7d83baadbeff75f0abcf
Thomas McKee <caffeine.storm@gmail.com>
2022-08-22 18:02:45 -0400

    Drop unused component

--- SCDPatientApp/components/StyledText.tsx
+++
@@ -1,7 +0,0 @@
-import * as React from 'react';
-
-import { Text, TextProps } from 'react-native';
-
-export function MonoText(props: TextProps) {
-  return <Text {...props} style={[props.style, { fontFamily: 'space-mono' }]} />;
-}
