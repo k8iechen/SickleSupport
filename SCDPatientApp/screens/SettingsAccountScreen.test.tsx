1c2b0f20aafb566c90b2bc8953833c4754208ef8
Thomas McKee <caffeine.storm@gmail.com>
2022-10-06 15:42:04 -0400

    Refactor out a helper to render native-base components in tests

--- SCDPatientApp/screens/SettingsAccountScreen.test.tsx
+++ SCDPatientApp/screens/SettingsAccountScreen.test.tsx
@@ -1,25 +1,10 @@
 import * as React from 'react';
 
 import {
-  NativeBaseProvider
-} from 'native-base';
-import {
-  render as native_render,
-} from '@testing-library/react-native';
+  render,
+} from '../testing/UITesting';
 
 import SettingsAccountScreen from './SettingsAccountScreen';
-
-const render = (comp) => {
-  const inset = {
-    frame: { x: 0, y: 0, width: 0, height: 0 },
-    insets: { top: 0, left: 0, right: 0, bottom: 0 },
-  };
-  return native_render(
-    <NativeBaseProvider initialWindowMetrics={inset}>
-      {comp}
-    </NativeBaseProvider>
-  );
-};
 
 test('can render', () => {
   const root = render(
--- SCDPatientApp/screens/SettingsScreen.test.tsx
+++ SCDPatientApp/screens/SettingsScreen.test.tsx
@@ -1,26 +1,14 @@
 import * as React from 'react';
 import {
-  NativeBaseProvider
-} from 'native-base';
-import {
   fireEvent,
-  render as native_render,
   screen,
 } from '@testing-library/react-native';
 
-import SettingsScreen from './SettingsScreen';
+import {
+  render,
+} from '../testing/UITesting';
 
-const render = (comp) => {
-  const inset = {
-    frame: { x: 0, y: 0, width: 0, height: 0 },
-    insets: { top: 0, left: 0, right: 0, bottom: 0 },
-  };
-  return native_render(
-    <NativeBaseProvider initialWindowMetrics={inset}>
-      {comp}
-    </NativeBaseProvider>
-  );
-};
+import SettingsScreen from './SettingsScreen';
 
 test('can render', () => {
   const navMock = {'navigate': jest.fn()};
