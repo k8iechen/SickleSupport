6fd2832197e6d5c400b5c8b81188d10c11a91319
Thomas McKee <caffeine.storm@gmail.com>
2022-10-10 19:28:03 -0400

    .gitignore a local data file for specifying debug settings

--- SCDPatientApp/src/common/DebugSettings.tsx
+++ SCDPatientApp/src/common/DebugSettings.tsx
@@ -1,8 +1,8 @@
-
-export default {
-  // `initialRouteName` maps to the route name to initally navigate to when
-  // opening the app. If you're manually testing a particular screen, you can
-  // save a lot of time by having the app open to that screen instead of
-  // re-navigating after every reload.
-  // "initialRouteName": "Home",
-};
+export default (() => {
+  try {
+    const result = require("./DebugSettings.data.tsx").default;
+    return result
+  } catch (e) {
+  }
+  return {};
+})();
