ba3669a971dbcceb1d110e3931341f6a9cdc6754
Thomas McKee <caffeine.storm@gmail.com>
2022-10-17 18:40:13 -0400

    Fix anonymous login

--- SCDPatientApp/src/navigation/index.tsx
+++ SCDPatientApp/src/navigation/index.tsx
@@ -31,7 +31,7 @@ const Navigation = observer(() => {
         },
       }}
     >
-      {authStore.isAuthenticated() ? (
+      {authStore.isAuthenticated() && authStore.getPatient().name ? (
         <Tabs />
       ) : (
         <OnboardingNavigator />
