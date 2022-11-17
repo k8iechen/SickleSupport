a64311e57c647c60353824cb834cc659d3a99bdc
Thomas McKee <caffeine.storm@gmail.com>
2022-11-17 13:54:12 -0500

    Refactor firestore imports

--- SCDPatientApp/src/stores/PainEntry.ts
+++ SCDPatientApp/src/stores/PainEntry.ts
@@ -1,20 +1,14 @@
 import { db } from "../firebase";
 import { TPatient } from "../models/Patient";
 import { TPainEntry } from "../models/PainEntry";
-import {
-  addDoc,
-  collection,
-  doc,
-  increment,
-  updateDoc,
-} from "firebase/firestore";
+import * as firestore from "firebase/firestore";
 
 export interface IPainEntryStore {
   addEntry: (
     patient: TPatient | null | undefined,
     entry: TPainEntry
   ) => Promise<boolean>;
-}
+};
 
 const PainEntryStore = (): IPainEntryStore => {
   const store: IPainEntryStore = {
@@ -25,18 +19,18 @@ const PainEntryStore = (): IPainEntryStore => {
       try {
         const patientId = patient?.uid?.toString();
         if (patientId) {
-          await addDoc(
-            collection(db, "patients", patientId, "pain_entries"),
+          await firestore.addDoc(
+            firestore.collection(db, "patients", patientId, "pain_entries"),
             entry
           );
 
-          const patientRef = doc(db, "patients", patientId);
-          await updateDoc(patientRef, {
-            pain_episodes: increment(1),
+          const patientRef = firestore.doc(db, "patients", patientId);
+          await firestore.updateDoc(patientRef, {
+            pain_episodes: firestore.increment(1),
           });
           if (entry.hospital_visit) {
-            await updateDoc(patientRef, {
-              hospital_visit: increment(1),
+            await firestore.updateDoc(patientRef, {
+              hospital_visit: firestore.increment(1),
             });
           }
           return true;
