32de6f1fdeca5c220d9bb3972803ac5af3b4699a
Thomas McKee <caffeine.storm@gmail.com>
2022-11-22 14:30:33 -0500

    ran eslint for PainEntry

--- SCDPatientApp/src/stores/PainEntry.test.ts
+++ SCDPatientApp/src/stores/PainEntry.test.ts
@@ -1,33 +1,29 @@
-import PainEntryStore, { IBackend } from "./PainEntry";
+import { Timestamp } from 'firebase/firestore';
+import PainEntryStore, { IBackend } from './PainEntry';
 
-import type { TPainEntry } from "../models/PainEntry";
-import type { TPatient } from "../models/Patient";
-import { Timestamp } from "firebase/firestore";
+import type { TPainEntry } from '../models/PainEntry';
+import type { TPatient } from '../models/Patient';
 
 // TODO: move this to models/test/Patient.ts
-const fakePatient = (): TPatient => {
-  return {
-    uid: 'uid',
-    name: 'name',
-  };
-};
-const fakePainEntry = (): TPainEntry => {
-  return {
-    created_at: Timestamp.fromMillis(42),
-    updated_at: Timestamp.fromMillis(1024),
-    pain_intensity: 0,
-    medication_taken: false,
-    tylenols_taken: 0,
-    anti_inflammatories_taken: 0,
-    short_acting_opiods_taken: 0,
-    long_acting_opiods_taken: 0,
-    location: "location",
-    pain_triggers: [],
-    relief_methods: [],
-    hospital_visit: false,
-    notes: "notes",
-  };
-};
+const fakePatient = (): TPatient => ({
+  uid: 'uid',
+  name: 'name',
+});
+const fakePainEntry = (): TPainEntry => ({
+  created_at: Timestamp.fromMillis(42),
+  updated_at: Timestamp.fromMillis(1024),
+  pain_intensity: 0,
+  medication_taken: false,
+  tylenols_taken: 0,
+  anti_inflammatories_taken: 0,
+  short_acting_opiods_taken: 0,
+  long_acting_opiods_taken: 0,
+  location: 'location',
+  pain_triggers: [],
+  relief_methods: [],
+  hospital_visit: false,
+  notes: 'notes',
+});
 
 test('addEntry will forward to backend', async () => {
   const backend = {
--- SCDPatientApp/src/stores/PainEntry.ts
+++ SCDPatientApp/src/stores/PainEntry.ts
@@ -1,14 +1,14 @@
-import { db } from "../firebase";
-import type { TPatient } from "../models/Patient";
-import type { TPainEntry } from "../models/PainEntry";
-import * as firestore from "firebase/firestore";
+import * as firestore from 'firebase/firestore';
+import { db } from '../firebase';
+import type { TPatient } from '../models/Patient';
+import type { TPainEntry } from '../models/PainEntry';
 
 export interface IPainEntryStore {
   addEntry: (
     patient: TPatient | null | undefined,
     entry: TPainEntry
   ) => Promise<boolean>;
-};
+}
 
 export interface IBackend {
   collection: typeof firestore.collection,
@@ -16,25 +16,25 @@ export interface IBackend {
   doc: typeof firestore.doc,
   updateDoc: typeof firestore.updateDoc,
   increment: typeof firestore.increment,
-};
+}
 
 const PainEntryStore = (backend?: IBackend): IPainEntryStore => {
-  backend = backend || firestore!;
+  backend = backend || firestore;
 
   const store: IPainEntryStore = {
     addEntry: async (
       patient: TPatient | null | undefined,
-      entry: TPainEntry
+      entry: TPainEntry,
     ): Promise<boolean> => {
       try {
         const patientId = patient?.uid?.toString();
         if (patientId) {
           await backend.addDoc(
-            backend.collection(db, "patients", patientId, "pain_entries"),
-            entry
+            backend.collection(db, 'patients', patientId, 'pain_entries'),
+            entry,
           );
 
-          const patientRef = backend.doc(db, "patients", patientId);
+          const patientRef = backend.doc(db, 'patients', patientId);
           await backend.updateDoc(patientRef, {
             pain_episodes: backend.increment(1),
           });
@@ -44,9 +44,8 @@ const PainEntryStore = (backend?: IBackend): IPainEntryStore => {
             });
           }
           return true;
-        } else {
-          throw "Error: there is no patient in the auth context";
         }
+        throw 'Error: there is no patient in the auth context';
       } catch (error) {
         console.log(error);
         return false;
